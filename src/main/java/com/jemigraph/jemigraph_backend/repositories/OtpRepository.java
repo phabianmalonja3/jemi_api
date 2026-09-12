e881e5e503c7336526254c4171de3abe0607392d
phabianmalonja3 <phabianmalonja3@gmail.com>
2026-09-12 11:20:27 +0300

    implemented of security over the system

--- src/main/java/com/jemigraph/jemigraph_backend/repositories/OtpRepository.java
+++ src/main/java/com/jemigraph/jemigraph_backend/repositories/OtpRepository.java
@@ -5,16 +5,22 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface OtpRepository extends JpaRepository<OtpVerification, UUID> {
 
+  @Modifying
+  @Transactional
   void deleteByExpiryTimeBefore(LocalDateTime now);
 
   Optional<OtpVerification> findTopByEmailOrderByIdDesc(String email);
 
   OtpVerification findByEmail(String email);
 
+  @Modifying
+  @Transactional
   void deleteByEmail(String toEmail);
 }
